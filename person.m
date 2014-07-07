9c71b1688a570c04a4c7f5ea6db78cecde187ba0
shacker <hacker65536@users.noreply.github.com>
2014-07-07 16:54:33 +0900

    Update person.m

--- person.m
+++ person.m
@@ -1,3 +1,23 @@
 #import "Person.h"
 @implementation Person
+// 名前を取得する
+- (NSString *)name
+{
+    return _name;
+}
+// 名前を設定する
+- (void)setName:(NSString *)name
+{
+    _name = name;
+}
+// 年齢を取得する
+- (NSInteger)age
+{
+    return _age;
+}
+// 年齢を設定する
+- (void)setAge:(NSInteger)age
+{
+    _age = age;
+}
 @end
