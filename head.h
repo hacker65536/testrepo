a9011bd4cffaec66faa04dd72422c3c66b9a3c4d
shacker <hacker65536@users.noreply.github.com>
2014-07-07 16:54:11 +0900

    Update head.h

--- head.h
+++ head.h
@@ -1,2 +1,19 @@
+#import <Foundation/Foundation.h>
 @interface Person : NSObject
+{
+    // 名前
+    NSString *_name;
+    
+    // 年齢
+    NSInteger _age;
+}
+// 名前を取得する
+- (NSString *)name;
+// 名前を設定する
+- (void)setName:(NSString *)name;
+ 
+// 年齢を取得する
+- (NSInteger)age;
+// 年齢を設定する
+- (void)setAge:(NSInteger)age;
 @end
